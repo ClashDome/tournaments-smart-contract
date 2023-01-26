cfd22ed8f250847e4b92a24728cb1b6490ed30a5
Escrichee <davidescriche6@gmail.com>
2023-01-26 12:00:17 +0100

    tournaments, creators and config created

--- include/clashdometrn.hpp
+++ include/clashdometrn.hpp
@@ -1,11 +1,133 @@
 #include <eosio/eosio.hpp>
+#include <eosio/singleton.hpp>
+#include <eosio/asset.hpp>
+#include <eosio/system.hpp>
+#include <eosio/crypto.hpp>
+
+
 using namespace eosio;
+using namespace std;
+
+#define EOSIO eosio::name("eosio")
+#define CONTRACTN eosio::name("clashdometrn")
 
 CONTRACT clashdometrn : public contract {
+
    public:
+
       using contract::contract;
 
-      ACTION hi( name nm );
+      ACTION initconfig();
 
-      using hi_action = action_wrapper<"hi"_n, &clashdometrn::hi>;
+      ACTION removeconfig();
+
+      ACTION addconftoken(
+         name token_contract, 
+         symbol token_symbol,
+         string type
+      );
+
+      ACTION createtrn(
+         name creator,
+         string name,
+         uint64_t timestamp_start, 
+         uint64_t duration_hours,
+         asset requeriment_fee,
+         asset requeriment_stake,
+         string requeriment_nft,
+         asset prize_pot,
+         string type_prize_pot,
+         bool recreate
+      );
+
+      ACTION canceltrn(
+         name creator,
+         uint64_t tournament_id
+      );
+
+      ACTION addcreator(
+         name creator,
+         string img,
+         vector <extended_symbol> supported_tokens_fee,
+         bool stake_available,
+         vector <extended_symbol> supported_tokens_stake,
+         bool nft_available,
+         bool pot_available
+      );
+
+      ACTION logcreatetrn(
+         uint64_t tournament_id, 
+         name creator,
+         string name,
+         uint64_t timestamp_start, 
+         uint64_t duration_hours,
+         asset requeriment_fee,
+         asset requeriment_stake,
+         string requeriment_nft,
+         asset prize_pot,
+         string type_prize_pot,
+         bool recreate
+      );
+
+   private:
+
+      // TABLES 
+
+      // tournaments
+      TABLE tournaments_s {
+        
+         uint64_t tournament_id;
+         name creator;
+         string name;
+         uint64_t timestamp_start; 
+         uint64_t duration_hours;
+         asset requeriment_fee;
+         asset requeriment_stake;
+         string requeriment_nft;
+         asset prize_pot;
+         string type_prize_pot;
+         bool recreate;
+
+         uint64_t primary_key() const { return tournament_id; }
+      };
+
+      typedef multi_index<name("tournaments"), tournaments_s> tournaments_t;
+    
+      tournaments_t tournaments = tournaments_t(get_self(), get_self().value);
+
+      // creators
+      TABLE creators_s {
+        
+         name creator;
+         string img;
+         vector <extended_symbol> supported_tokens_fee;
+         bool stake_available;
+         vector <extended_symbol> supported_tokens_stake;
+         bool nft_available;
+         bool pot_available;
+
+         uint64_t primary_key() const { return creator.value; }
+      };
+
+      typedef multi_index<name("creators"), creators_s> creators_t;
+    
+      creators_t creators = creators_t(get_self(), get_self().value);
+
+      // config
+      TABLE config_s {
+         uint64_t tournament_counter = 1;
+         vector <extended_symbol> supported_tokens_fee = {};
+         vector <extended_symbol> supported_tokens_stake = {};
+      };
+
+      typedef singleton <name("config"), config_s> config_t;
+      config_t config = config_t(get_self(), get_self().value);
+      typedef multi_index <name("config"), config_s> config_t_for_abi;
+
+      // BLOCKCHAIN TOKENS
+      const string EOSIO_CONTRACT = "eosio.token";
+      static constexpr symbol WAX_SYMBOL = symbol(symbol_code("WAX"), 8);
+
+      const string ALIEN_WORLDS_CONTRACT = "alien.worlds";
+      static constexpr symbol TLM_SYMBOL = symbol(symbol_code("TLM"), 4);
 };
--- src/clashdometrn.cpp
+++ src/clashdometrn.cpp
@@ -1,5 +1,208 @@
 #include <clashdometrn.hpp>
-ACTION clashdometrn::hi( name nm ) {
-   /* fill in action body */
-   print_f("Name : %\n",nm);
+
+/**
+*  Initializes the config table. Only needs to be called once when first deploying the contract
+*  @required_auth The contract itself
+*/
+ACTION clashdometrn::initconfig() 
+{
+
+   require_auth(get_self());
+
+   config.get_or_create(get_self(), config_s{});
+}
+
+/**
+*  Remove the config table
+*  @required_auth The contract itself
+*/
+ACTION clashdometrn::removeconfig() 
+{
+
+   require_auth(get_self());
+
+   config.remove();
+}
+
+/**
+*  Adds a token that can then be used as fee or stake in tournaments
+*  @required_auth The contract itself
+*/
+ACTION clashdometrn::addconftoken(
+   name token_contract, 
+   symbol token_symbol,
+   string type) 
+{
+   require_auth(get_self());
+
+   config_s current_config = config.get();
+
+   if (type == "fee") {
+      for (extended_symbol token : current_config.supported_tokens_fee) {
+         check(token.get_symbol() != token_symbol, "A token with this symbol is already supported");
+      }
+      current_config.supported_tokens_fee.push_back(extended_symbol(token_symbol, token_contract));
+   } else {
+      for (extended_symbol token : current_config.supported_tokens_stake) {
+         check(token.get_symbol() != token_symbol, "A token with this symbol is already supported");
+      }
+      current_config.supported_tokens_stake.push_back(extended_symbol(token_symbol, token_contract));
+   }
+   
+
+   config.set(current_config, get_self());
+}
+
+/**
+*  Creates a new tournament
+*  @required_auth The tournament creator
+*/
+ACTION clashdometrn::createtrn(
+   name creator,
+   string name,
+   uint64_t timestamp_start, 
+   uint64_t duration_hours,
+   asset requeriment_fee,
+   asset requeriment_stake,
+   string requeriment_nft,
+   asset prize_pot,
+   string type_prize_pot,
+   bool recreate
+) {
+   
+   // TODO: auth with custom keys
+   // internal_use_do_not_use::require_auth2(creator.value, "one"_n.value);
+   require_auth(creator);
+
+   // TODO: comprobar timestamps por si se tiene otro torneo ya en curso
+
+   // TODO comprobar si es un creador especial y fee, stake y pot
+
+   // check fee and stake symbols
+   config_s current_config = config.get();
+
+   bool is_fee_supported = false;
+   
+   for (extended_symbol supported_token_fee : current_config.supported_tokens_fee) {
+      if (supported_token_fee.get_symbol() == requeriment_fee.symbol) {
+         is_fee_supported = true;
+         break;
+      }
+   }
+
+   check(is_fee_supported, "The specified stake symbol is not supported");
+
+   bool is_stake_supported = false;
+   
+   for (extended_symbol supported_token_stake : current_config.supported_tokens_stake) {
+      if (supported_token_stake.get_symbol() == requeriment_stake.symbol) {
+         is_stake_supported = true;
+         break;
+      }
+   }
+
+   check(is_stake_supported, "The specified stake symbol is not supported");
+
+   uint64_t tournament_id = current_config.tournament_counter++;
+   config.set(current_config, get_self());
+
+   tournaments.emplace(CONTRACTN, [&](auto& trn) {
+      trn.tournament_id = tournament_id;
+      trn.creator = creator;
+      trn.name = name;
+      trn.timestamp_start = timestamp_start;
+      trn.duration_hours = duration_hours;
+      trn.requeriment_fee = requeriment_fee;
+      trn.requeriment_stake = requeriment_stake;
+      trn.requeriment_nft = requeriment_nft;
+      trn.prize_pot = prize_pot;
+      trn.type_prize_pot = type_prize_pot;
+      trn.recreate = recreate;
+   });
+
+   action(
+      permission_level{get_self(), eosio::name("active")},
+      get_self(),
+      eosio::name("logcreatetrn"),
+      make_tuple(
+         tournament_id,
+         creator,
+         name,
+         timestamp_start,
+         duration_hours,
+         requeriment_fee,
+         requeriment_stake,
+         requeriment_nft,
+         prize_pot,
+         type_prize_pot,
+         recreate
+      )
+   ).send();
+}
+
+/**
+*  Adds a token that can then be used in tournaments
+*  @required_auth The tournament creator
+*/
+ACTION clashdometrn::canceltrn(
+   name creator,
+   uint64_t tournament_id
+)
+{
+
+   require_auth(creator);
+
+   auto trn_itr = tournaments.require_find(tournament_id, "No tournament with this id exists");
+   check(trn_itr->creator == creator, "The specified account isn't the creator of the tournament.");
+
+   tournaments.erase(trn_itr);
+}
+
+/**
+*  Adds a special creator
+*  @required_auth The tournament creator
+*/
+ACTION clashdometrn::addcreator(
+   name creator,
+   string img,
+   vector <extended_symbol> supported_tokens_fee,
+   bool stake_available,
+   vector <extended_symbol> supported_tokens_stake,
+   bool nft_available,
+   bool pot_available
+)
+{
+
+   require_auth(get_self());
+
+   check(creators.find(creator.value) == creators.end(), "Creator " + creator.to_string() + " already exists.");
+
+   creators.emplace(CONTRACTN, [&](auto& crt) {
+      crt.creator = creator;
+      crt.img = img;
+      crt.supported_tokens_fee = supported_tokens_fee;
+      crt.stake_available = stake_available;
+      crt.supported_tokens_stake = supported_tokens_stake;
+      crt.nft_available = nft_available;
+      crt.pot_available = pot_available;
+   });
+}
+
+// LOG ACTIONS
+
+ACTION clashdometrn::logcreatetrn(
+   uint64_t tournament_id, 
+   name creator,
+   string name,
+   uint64_t timestamp_start, 
+   uint64_t duration_hours,
+   asset requeriment_fee,
+   asset requeriment_stake,
+   string requeriment_nft,
+   asset prize_pot,
+   string type_prize_pot,
+   bool recreate
+) {
+   
+   require_auth(get_self());
 }
